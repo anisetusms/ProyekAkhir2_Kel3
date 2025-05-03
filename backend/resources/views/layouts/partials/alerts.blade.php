0f1075c8d6f68bbed075a5ea44222aee766495bc
Anisetus  Bambang Manalu <anisetusm@gmail.com>
2025-05-03 21:29:19 +0700

    gunadi perusak file

--- backend/resources/views/layouts/partials/alerts.blade.php
+++
@@ -1,6 +0,0 @@
-@if (session('info'))
-    <div class="alert alert-info alert-dismissible fade show" role="alert">
-        {{ session('info') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endif
