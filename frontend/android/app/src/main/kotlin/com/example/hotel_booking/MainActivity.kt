efb96f13cacba9a5a2c79422792a432b215b2498
Anisetus  Bambang Manalu <anisetusm@gmail.com>
2025-05-15 16:30:54 +0700

    folder rusak

--- frontend/android/app/src/main/kotlin/com/example/hotel_booking/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hotel_booking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/lib/presentation/screen/bookmark/bookmark_controller.dart
+++
@@ -1,8 +0,0 @@
-part of 'bookmark_import.dart';
-
-class BookMarkController extends GetxController {
-  ThemeController themeController = Get.put(ThemeController()) ;
-  HomeController homeController = HomeController();
-
-  RxInt selectedButton = 0.obs;
-}
